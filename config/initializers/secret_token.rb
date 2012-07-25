d7b3f2cba3a912100dcfa1f372a36a86ec690ebe
Marcelo De Polli <marcelo.polli@gmail.com>
2012-07-25 18:11:32 -0300

    New initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LivroDaClasse::Application.config.secret_token = '296994ab2a401c7dddc2a8957513c989f80b87f3a736af40bdb70f63b00860377d2259096af70d2390f9e2b08b7806183f8f5fa26629a7e8582e176223776478'
