73fd3f5fa7d69eeff71cb273bfa134a553c0e5db
Marcelo De Polli <marcelo.polli@gmail.com>
2012-08-02 17:32:45 -0300

    New controllers+views working :)

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ActiveRecord::Base
   attr_accessible :published_at, :title
   has_and_belongs_to_many :texts
   belongs_to :school, :inverse_of => :books
+
+  def to_label
+    "#{title} (#{texts.count})"
+  end
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,5 +1,5 @@
 class Text < ActiveRecord::Base
-  attr_accessible :content, :title
+  attr_accessible :content, :title, :book_ids
   has_and_belongs_to_many :books
   has_and_belongs_to_many :person
 end
