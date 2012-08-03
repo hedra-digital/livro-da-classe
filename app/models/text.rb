d6e647dcbd577e7675bdb6f4e5e0a0ec83d6375b
Marcelo De Polli <marcelo.polli@gmail.com>
2012-08-03 06:43:31 -0300

    Made the book views look like the text views

--- app/models/text.rb
+++ app/models/text.rb
@@ -2,4 +2,8 @@ class Text < ActiveRecord::Base
   attr_accessible :content, :title, :book_ids
   has_and_belongs_to_many :books
   has_and_belongs_to_many :person
+
+  def to_label
+    "<em>#{title}</em>"
+  end
 end
