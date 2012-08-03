b4dbe3e12a2d7bb598e6f66596a7a0fd9bbd1101
Marcelo De Polli <marcelo.polli@gmail.com>
2012-08-03 00:40:26 -0300

    Even better Bootstrap implementation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-  attr_accessible :published_at, :title
+  attr_accessible :published_at, :title, :text_ids
   has_and_belongs_to_many :texts
   belongs_to :school, :inverse_of => :books
 
--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -55,7 +55,7 @@ SimpleForm.setup do |config|
   # config.boolean_style = :nested
 
   # Default class for buttons
-  config.button_class = 'btn'
+  config.button_class = 'btn btn-primary'
 
   # Method used to tidy up errors. Specify any Rails Array method.
   # :first lists the first message for each field.
@@ -98,7 +98,7 @@ SimpleForm.setup do |config|
   config.label_class = 'control-label'
 
   # You can define the class to use on all forms. Default is simple_form.
-  # config.form_class = :simple_form
+  config.form_class = 'form-horizontal'
 
   # You can define which elements should obtain additional classes
   # config.generate_additional_classes_for = [:wrapper, :label, :input]
