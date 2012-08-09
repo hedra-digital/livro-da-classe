3fc2bde1136c78b98b8615f3c6340e9c6ff9f98e
Marcelo De Polli <marcelo.polli@gmail.com>
2012-08-09 16:52:29 -0300

    Implementing LaTeX support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+def kramdown(text)
+	return Kramdown::Document.new(text).to_latex
+end
+
+def lesc(text)
+  LatexToPdf.escape_latex(text)
+end
+
 end
--- app/helpers/kramdown_helper.rb
+++
@@ -1,7 +0,0 @@
-module KramdownHelper
-
-	def kramdown(text)
-		return Kramdown::Document.new(text).to_latex
-	end
-
-end
