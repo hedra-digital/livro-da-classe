ddfa7d263d3b97da464a4848efb34414a16262c8
Marcelo De Polli <marcelo.polli@gmail.com>
2012-08-13 18:36:11 -0300

    Fixed application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
 
+def full_text(book)
+	@book.texts.each do |text|
+		"\chapter{#{text.title}"
+		kramdown text.content
+	end
+end
+
 def kramdown(text)
 	return Kramdown::Document.new(text).to_latex
 end
