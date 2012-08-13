dc7c82e4f2788c447c030d1c01317c90c0e807a1
Marcelo De Polli <marcelo.polli@gmail.com>
2012-08-13 20:56:01 -0300

    Better gem integration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
 
 def full_text(book)
-	@book.texts.each do |text|
-		"\chapter{#{text.title}"
-		kramdown text.content
+	a = String.new
+	book.texts.each do |text|
+		a << "\\chapter{#{text.title}}\n#{k_to_latex(text.content)}\n"
 	end
+	a
 end
 
-def kramdown(text)
+def k_to_latex(text)
 	return Kramdown::Document.new(text).to_latex
 end
 
