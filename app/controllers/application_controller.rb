4a30123ee54b86a4cef5b833b867ffc08b9290d3
Marcelo De Polli <marcelo.polli@gmail.com>
2012-08-18 16:17:57 -0300

    Added school + book on teacher's header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+	protect_from_forgery
 
-  def logout
-	session['admin_logged']     = nil;
-	session['professor_logged'] = nil;
-	session['student_logged']   = nil;
-	redirect_to "/"
-  end
+	def logout
+		session['admin_logged']     = nil;
+		session['professor_logged'] = nil;
+		session['student_logged']   = nil;
+		redirect_to "/"
+	end
 
-  private
-    def authentication_check
-      authenticate_or_request_with_http_basic do |user, password|
-        user == "admin" && password == "hedra21"
-      end
-    end
+	private
+		def authentication_check
+			authenticate_or_request_with_http_basic do |user, password|
+				user == "admin" && password == "hedra21"
+			end
+		end
 end
-
