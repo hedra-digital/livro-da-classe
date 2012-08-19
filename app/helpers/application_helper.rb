c6de6e13e8b0a12bbc43af77f7e1eb1adc143b2c
Rodrigo Franco <caffeine@gmail.com>
2012-08-18 22:38:34 -0300

    Teacher navbar added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,12 @@ def logged_in_as
 	end
 end
 
+def nav_link(link_text, link_path)
+  class_name = current_page?(link_path) ? 'active' : ''
+
+  content_tag(:li, :class => class_name) do
+    link_to link_text, link_path
+  end
+end
+
 end
