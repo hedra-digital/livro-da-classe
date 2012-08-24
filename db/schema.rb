4ea84aef765deb32840f4f03534885240d524d20
Marcelo De Polli <marcelo.polli@gmail.com>
2012-08-24 10:27:54 -0300

    Bumped version on schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120819193024) do
+ActiveRecord::Schema.define(:version => 20120824130402) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
