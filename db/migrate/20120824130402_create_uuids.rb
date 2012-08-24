70d9970e1f87f47bdcb0eeb27ae512fdd8f3e54a
Marcelo De Polli <marcelo.polli@gmail.com>
2012-08-24 11:21:32 -0300

    Missed this migration

--- db/migrate/20120824130402_create_uuids.rb
+++ db/migrate/20120824130402_create_uuids.rb
@@ -1,7 +1,11 @@
 class CreateUuids < ActiveRecord::Migration
-  def up
-  end
+	def change
+		create_table "uuids", :force => true do |t|
+			t.string  "uuid"
+			t.integer "uuidable_id"
+			t.string  "uuidable_type", :limit => 40
+		end
 
-  def down
-  end
+		add_index "uuids", ["uuidable_id", "uuidable_type"], :name => "index_uuids_on_uuidable_id_and_uuidable_type"
+	end
 end
