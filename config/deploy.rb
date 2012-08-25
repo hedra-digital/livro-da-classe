ffaaee41825df3018ff192d82e26f5d21215b98a
Marcelo De Polli <marcelo.polli@gmail.com>
2012-08-25 02:07:30 -0300

    Added local precompile task to config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,3 +51,15 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
 end
+
+namespace :deploy do
+  namespace :assets do
+    desc "Precompile assets on local machine and upload them to the server."
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      run_locally "bundle exec rake assets:precompile"
+      find_servers_for_task(current_task).each do |server|
+        run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
+      end
+    end
+  end
+end
