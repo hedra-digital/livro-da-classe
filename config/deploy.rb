6fca5549387717577b5bdec7df0a009f8e56b043
Rodrigo Franco <caffeine@gmail.com>
2012-09-05 22:28:31 -0300

    Solving california uber alles

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,14 +52,14 @@ namespace :deploy do
   end
 end
 
-namespace :deploy do
-  namespace :assets do
-    desc "Precompile assets on local machine and upload them to the server."
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      run_locally "bundle exec rake assets:precompile"
-      find_servers_for_task(current_task).each do |server|
-        run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
-      end
-    end
-  end
-end
+# namespace :deploy do
+#   namespace :assets do
+#     desc "Precompile assets on local machine and upload them to the server."
+#     task :precompile, :roles => :web, :except => { :no_release => true } do
+#       run_locally "bundle exec rake assets:precompile"
+#       find_servers_for_task(current_task).each do |server|
+#         run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
+#       end
+#     end
+#   end
+# end
