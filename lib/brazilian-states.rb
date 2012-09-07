c2444aa447b597ab47fdc4d8d1605663865c6b27
Marcelo De Polli <marcelo.polli@gmail.com>
2012-09-07 13:20:31 -0300

    Working BrazilianStates with values and labels

--- lib/brazilian-states.rb
+++ lib/brazilian-states.rb
@@ -2,10 +2,9 @@
 
 require "rubygems"
 require "bundler/setup"
-
 require "json"
 
-class BrazilianStates < Object  
+class BrazilianStates < Object
   
   def self.all
     @all ||= JSON.parse(File.open("#{Rails.root}/lib/states.json").read) 
@@ -13,7 +12,7 @@ class BrazilianStates < Object
   end
 
   def self.names
-    all.map{|x| x["name"] }
+    all.map{|x| [x["name"].titleize + " - " + x["acronym"].upcase, x["name"]]}
   end
 
   def self.find_by_name(name)
@@ -22,9 +21,20 @@ class BrazilianStates < Object
 
   def self.find_cities_by_state(name)
     if name.present?
-      find_by_name(name.downcase)["cities"]
+      find_by_name(name.downcase)["cities"].map{|x| [x.titleize, x]}
     else
       return []
     end
   end
+
+  private
+
+  def titleize_do_b(word)
+    humanize(underscore(word)).gsub(/(?!de|da|do\b)\b\w+/) { $1.capitalize }
+  end
+
+  def titular(word)
+    humanize(underscore(word)).gsub(/\b('?[a-z])/) { $1.capitalize }
+  end
+
 end
