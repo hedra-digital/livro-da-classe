4654b66cd868040a47598758f983a97de68a9f15
Marcelo De Polli <marcelo.polli@gmail.com>
2012-09-07 06:33:25 -0300

    Added Fabio's address

--- app/mailers/request_contact.rb
+++ app/mailers/request_contact.rb
@@ -7,7 +7,7 @@ class RequestContact < ActionMailer::Base
 	def report_lead(lead_info)
 		@lead_info = lead_info
 		mail(
-			:to => ["marcelo.polli@gmail.com", "marcelo@gopubhub.com"],
+			:to => ["marcelo.polli@gmail.com", "fabio@hedra.com.br"],
 			:subject => "[Livro da Classe] Solicitação de contato",
 			:return_path => "marcelo@livrodaclasse.com.br",
 			:date => Time.now
