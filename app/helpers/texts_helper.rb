e3df6e509468a007ffe8f5510ce9e7d5c511b37d
Marcelo De Polli <marcelo.polli@gmail.com>
2012-09-12 11:31:08 -0300

    Added shortened text URL

--- app/helpers/texts_helper.rb
+++ app/helpers/texts_helper.rb
@@ -1,2 +1,10 @@
 module TextsHelper
+
+  def url_texto
+    url = Googl.shorten(book_text_url(@book.uuid, @text.uuid)).short_url
+    icon = content_tag :i, nil, :class => 'icon-globe'
+    link = link_to url, url
+    content_tag :p, icon + '&nbsp;URL do texto:&nbsp;'.html_safe + link
+  end
+
 end
