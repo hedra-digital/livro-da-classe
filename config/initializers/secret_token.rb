30c560621c3991df590ddc6dfc7f28971143e5f5
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-26 19:15:23 -0200

    Brand new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Livrodaclasse::Application.config.secret_token = '0d3126e41105c26a428449a731fb9542bdabdc57e07487f93eea2ca0ad3b6813dbe68397eefe1d0d020b64ea8c2b6374ac309b9b2892c7be82c3e1874c8cc072'
