bcf90b1d9849034e8a7fbf5d0d284624a54f96eb
Marcelo De Polli <marcelo.polli@gmail.com>
2012-12-29 00:02:21 -0200

    Add respond_to examples

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,28 +2,38 @@ require 'spec_helper'
 
 describe User do
 
-  it 'is invalid without a name' do
-    user = User.new(:email => 'test@example.com', :password_digest => 'test21')
-    user.should_not be_valid
-    expect { user.save! }.to raise_error(
-      ActiveRecord::RecordInvalid
-    )
-  end
+  it { should respond_to(:name) }
 
-  it 'is invalid without an email' do
-    user = User.new(:name => 'Joe Schmuck', :password_digest => 'test21')
-    user.should_not be_valid
-    expect { user.save! }.to raise_error(
-      ActiveRecord::RecordInvalid
-    )
-  end
+  it { should respond_to(:email) }
 
-  it 'is invalid without a password' do
-    user = User.new(:name => 'Joe Schmuck', :email => 'test@example.com')
-    user.should_not be_valid
-    expect { user.save! }.to raise_error(
-      ActiveRecord::RecordInvalid
-    )
+  it { should respond_to(:password_digest) }
+
+  context 'when validating' do
+
+    it 'is invalid without a name' do
+      user = User.new(:email => 'test@example.com', :password_digest => 'test21')
+      user.should_not be_valid
+      expect { user.save! }.to raise_error(
+        ActiveRecord::RecordInvalid
+      )
+    end
+
+    it 'is invalid without an email' do
+      user = User.new(:name => 'Joe Schmuck', :password_digest => 'test21')
+      user.should_not be_valid
+      expect { user.save! }.to raise_error(
+        ActiveRecord::RecordInvalid
+      )
+    end
+
+    it 'is invalid without a password' do
+      user = User.new(:name => 'Joe Schmuck', :email => 'test@example.com')
+      user.should_not be_valid
+      expect { user.save! }.to raise_error(
+        ActiveRecord::RecordInvalid
+      )
+    end
+
   end
 
 end
