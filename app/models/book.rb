5b9b8c001de3bbca98ff7b0ee3f2453e2f514c03
Rodrigo Franco <caffeine@gmail.com>
2013-01-17 01:07:46 -0200

    Converts Book#set_uuid to public

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,15 +7,15 @@ class Book < ActiveRecord::Base
 
   before_save             :set_uuid
 
-  private
-
-  def set_uuid
-     self.uuid = Guid.new.to_s if self.uuid.nil?
-  end
-
   def self.find_by_uuid_or_id(id)
     response   = Book.find_by_uuid(id.to_s)
     response ||= Book.find_by_id(id)
     return response
+  end
+
+  private
+
+  def set_uuid
+     self.uuid = Guid.new.to_s if self.uuid.nil?
   end
 end
