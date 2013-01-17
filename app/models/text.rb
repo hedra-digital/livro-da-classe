e3a6c5eae7782fa378b58b359143bb77eb31baeb
Rodrigo Franco <caffeine@gmail.com>
2013-01-17 01:08:08 -0200

    Implements UUID methods on Text model

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,6 +1,19 @@
 class Text < ActiveRecord::Base
   attr_accessible :book_id, :content, :title, :uuid, :content
   belongs_to :book
-  
-  validates :book_id, :presence => true
+
+  validates :book_id,     :presence => true
+  before_save             :set_uuid
+
+  def self.find_by_uuid_or_id(id)
+    response   = Text.find_by_uuid(id.to_s)
+    response ||= Text.find_by_id(id)
+    return response
+  end
+
+  private
+
+  def set_uuid
+     self.uuid = Guid.new.to_s if self.uuid.nil?
+  end
 end
--- spec/models/text_spec.rb
+++ spec/models/text_spec.rb
@@ -17,10 +17,36 @@ describe Text do
   end
 
   context 'when saving' do
+    it 'should trigger .set_uuid' do
+      text = create(:text, :uuid => nil)
+      text.uuid.should_not be_nil
+    end
+
     it 'should belongs to a book' do
       text = create(:text)
       text.book.should_not be_nil
       text.book.should be_an_instance_of(Book)
     end    
   end
+
+   context '.set_uuid' do
+    it 'should set instance uuid using a new Guid' do
+      text = build(:text, :uuid => nil)
+      text.send(:set_uuid)
+      text.uuid.should_not be_nil
+    end
+  end
+
+  context '#find_by_uuid_or_id' do
+    let(:text) {  create(:text) }
+
+    it 'should find text by id' do
+      Text.find_by_uuid_or_id(text.id).should_not be_nil
+    end
+
+    it 'should find text by uuid' do
+        Text.find_by_uuid_or_id(text.uuid).should_not be_nil
+    end
+  end
+
 end
