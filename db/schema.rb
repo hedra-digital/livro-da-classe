7b8cd986d700cb13420b943fe358bf9e9dd0e42a
Rodrigo Franco <caffeine@gmail.com>
2013-01-17 00:20:53 -0200

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130116034942) do
+ActiveRecord::Schema.define(:version => 20130117013321) do
+
+  create_table "books", :force => true do |t|
+    t.datetime "published_at"
+    t.string   "title"
+    t.string   "uuid"
+    t.string   "subtitle"
+    t.text     "organizers"
+    t.text     "directors"
+    t.text     "coordinators"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
