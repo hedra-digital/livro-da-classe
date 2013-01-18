534eba06c1c8ea7be875c601ea3d80eeba9eace7
Rodrigo Franco <caffeine@gmail.com>
2013-01-18 01:55:04 -0200

    Adds BookController routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Livrodaclasse::Application.routes.draw do
   resources :users
   resources :sessions
   resources :password_resets
+  resources :books
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
