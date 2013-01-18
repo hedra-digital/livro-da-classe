d44ecefa371c25307cd49e9fb5c8e529ff5aa52b
Rodrigo Franco <caffeine@gmail.com>
2013-01-18 01:55:27 -0200

    Ensure we validate the password presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
   # Validations
   validates           :name, :presence => true
   validates           :email, :email_format => { :message => 'Não é um formato válido de e-mail', :allow_blank => true }, :uniqueness => true, :presence => true
-
+  validates           :password, :presence => true, :on => :create
+  
   # Specify fields that can be accessible through mass assignment
   attr_accessible     :email, :name, :password, :password_confirmation, :educator, :student_count, :school_name
 
