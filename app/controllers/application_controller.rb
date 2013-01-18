f7f4616b334a4580236b9a85534516139c2bb4ab
Rodrigo Franco <caffeine@gmail.com>
2013-01-18 01:54:27 -0200

    Implements basic auth check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
+    session['current_user'] ||= User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
+  end
+
+  def authentication_check
+    unless  session['current_user'].present?
+      redirect_to signin_path
+    end
   end
 
   helper_method :current_user
