a27f1701745798aaf0f39b260eb00f58261a8c50
Rodrigo Franco <caffeine@gmail.com>
2013-01-18 01:54:51 -0200

    Adds required attributes to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,15 @@
 class Book < ActiveRecord::Base
-  attr_accessible :coordinators, :directors, :organizers, :published_at, :subtitle, :title, :uuid, :organizer
+  attr_accessible :coordinators, :directors, :organizers, :published_at, :subtitle, :title, :uuid, :organizer, :text_ids
   belongs_to :organizer, :class_name => "User"
 
   validates :organizer, :presence => true
   validates :title,     :presence => true
 
-  before_save             :set_uuid
+  before_save            :set_uuid
+
+  has_many              :texts
+
+  attr_accessor         :finished_at 
 
   def self.find_by_uuid_or_id(id)
     response   = Book.find_by_uuid(id.to_s)
