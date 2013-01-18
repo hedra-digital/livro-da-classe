a04810dff62f6140534222be8e89018a590389c9
Rodrigo Franco <caffeine@gmail.com>
2013-01-18 01:55:49 -0200

    Unlogged attempts to hit BookController#index should result in a 302

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 describe "Books" do
   describe "GET /books" do
     it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get books_path
-      response.status.should be(200)
+      response.status.should be(302)
     end
   end
 end
