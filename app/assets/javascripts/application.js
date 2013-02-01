36940027d7cceeb3e24815a3d9fa2501e4294fdb
Rodrigo Franco <caffeine@gmail.com>
2013-02-01 00:10:22 -0200

    Removes modernizr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require modernizr-2.6.2.min
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
