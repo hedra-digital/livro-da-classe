9e81cf4b9996e35680b2313a2cbc79282785557c
Rodrigo Franco <caffeine@gmail.com>
2013-02-09 01:11:51 -0200

    Add HedraLatex

---
+++ lib/hedra-latex.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+
+class HedraLatex < Kramdown::Converter::Latex
+
+
+  def convert_header(el, opts)
+    type = @options[:latex_headers][output_header_level(el.options[:level]) - 1]
+    return "\\#{type}*{#{inner(el, opts)}}\n\n"
+  end
+  
+end
