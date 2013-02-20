4e1b7ab8a41eee3e20b15225a91e37192141b62e
Marcelo De Polli <marcelo.polli@gmail.com>
2013-02-20 10:35:11 -0300

    Update factories/projects.rb

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :project do
     book_id ""
-    release_date ""
-    finish_date "2013-02-20"
+    release_date 3.months.since
+    finish_date 3.months.since - 2.weeks
   end
 end
