5474c4e41b57e9446cadec36097d85efbac916db
Marcelo De Polli <marcelo.polli@gmail.com>
2013-02-24 18:33:38 -0300

    Fix flash notice HTML bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     flash.inject("") do |sum, message|
       content_tag :div, :class => "alert alert-#{types[message[0]]}" do
         button_tag('&#215;'.html_safe, :type => 'button', :class => 'close', :'data-dismiss' => 'alert', :name => nil) +
-        message[1]
+        message[1].html_safe
       end
     end
   end
