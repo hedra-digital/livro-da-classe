951b1f2dbb9ddc34e3fa8d8b9610a2b3ba2375bf
Marcelo De Polli <marcelo.polli@gmail.com>
2013-02-26 09:34:49 -0300

    Set OmniAuth providers according to Rails.env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,15 @@
 OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "2byTz4MOQIyv3YoMHzFVEQ", "cRU6vQSh7pg5yZxRRPSu1HnSFNDSZI4SDmeo0ZcVV5I"
-  provider :facebook, "462817203782895", "6ad0fab174bcd0d298195957fe68a48c"
-  provider :google_oauth2, "1087006017149.apps.googleusercontent.com", "QrdEkoAt1AURclhddV7a8GP5"
+if Rails.env.staging?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, "2byTz4MOQIyv3YoMHzFVEQ", "cRU6vQSh7pg5yZxRRPSu1HnSFNDSZI4SDmeo0ZcVV5I"
+    provider :facebook, "462817203782895", "6ad0fab174bcd0d298195957fe68a48c"
+    provider :google_oauth2, "1087006017149-t8ad3ej20ib46kqs33638avmsu7kr3fo.apps.googleusercontent.com", "Z1C7hirHxUe25zgLq5oGshsC"
+  end
+elsif Rails.env.development?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, "2byTz4MOQIyv3YoMHzFVEQ", "cRU6vQSh7pg5yZxRRPSu1HnSFNDSZI4SDmeo0ZcVV5I"
+    provider :facebook, "462817203782895", "6ad0fab174bcd0d298195957fe68a48c"
+    provider :google_oauth2, "1087006017149.apps.googleusercontent.com", "QrdEkoAt1AURclhddV7a8GP5"
+  end
 end
