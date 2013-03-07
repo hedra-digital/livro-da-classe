2c63a3301e418801f9eb372336d5028a865884bc
Marcelo De Polli <marcelo.polli@gmail.com>
2013-03-07 12:00:54 -0300

    Add active menu item helper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -18,4 +18,10 @@ module BooksHelper
     end
     tags.html_safe
   end
+
+  def menu_item(name, path)
+    options = {}
+    options[:class] = 'active' if current_page?(path)
+    content_tag(:li, link_to(name, path), options)
+  end
 end
