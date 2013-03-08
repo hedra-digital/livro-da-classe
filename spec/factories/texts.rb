e760472812b2b4f320081e7efcb7eb103c201034
Marcelo De Polli <marcelo.polli@gmail.com>
2013-03-08 19:42:12 -0300

    Fix text factory

--- spec/factories/texts.rb
+++ spec/factories/texts.rb
@@ -2,6 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :text do
+    book
     title { Faker::Lorem.sentence }
     uuid "6ce94f7e-2c64-a1df-e9d6-a1211491ad72"
     content { Faker::Lorem.paragraph }
