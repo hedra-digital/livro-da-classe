bef5929ed6022847041e715337f4f1705c66f41f
Marcelo De Polli <marcelo.polli@gmail.com>
2013-04-22 18:51:02 -0300

    Add omniauth API info for 7letras

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -18,4 +18,10 @@ elsif Rails.env.staging? || Rails.env.test? || Rails.env.development?
     provider :facebook, "462817203782895", "6ad0fab174bcd0d298195957fe68a48c"
     provider :google_oauth2, "1087006017149-trcfrjjs4fg5ftjsmp8dpd1oai84vifu.apps.googleusercontent.com", "1maEZuCXHaFwYiQF_cXIjBY2"
   end
+elsif Rails.env.7letras?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :twitter, "NnjbyC3WP9r8sJKnnRJDw", "Nha7HvAy95QdY8SsT45HHYhDTbmK53XI5wMkChLMyzI"
+    provider :facebook, "462817203782895", "6ad0fab174bcd0d298195957fe68a48c"
+    provider :google_oauth2, "1087006017149-2b0eihce0m6ft4eh5ie43umfotd3fi4o.apps.googleusercontent.com", "cica4cyWsWDmF_4HIQKtjALC"
+  end
 end
