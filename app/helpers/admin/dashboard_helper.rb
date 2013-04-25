0df918575fd7deb30261cb277688e6960186ad60
Marcelo De Polli <marcelo.polli@gmail.com>
2013-04-25 11:50:46 -0300

    Fix admin project status helper

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,2 +1,10 @@
 module Admin::DashboardHelper
+
+  def admin_book_status(book)
+    tags = ""
+    if book.project.present?
+      tags << remaining_label(book.project)
+    end
+    tags.html_safe
+  end
 end
