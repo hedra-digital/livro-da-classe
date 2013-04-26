1e79cd759114ef19cfb020948d3bcca4d843d487
Marcelo De Polli <marcelo.polli@gmail.com>
2013-04-25 22:55:52 -0300

    Add basic HTTP auth only for selected environments

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,8 +1,7 @@
 class Admin::ApplicationController < ActionController::Base
-  protect_from_forgery
-
-  http_basic_authenticate_with :name => CONFIG[:admin_username], :password => CONFIG[:admin_password]
-
   layout 'admin/layouts/application'
 
+  protect_from_forgery
+
+  http_basic_authenticate_with(:name => CONFIG[:admin_username], :password => CONFIG[:admin_password]) if ['staging', 'sales'].include?(Rails.env)
 end
