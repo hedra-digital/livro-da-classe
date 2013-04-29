e65488e5bb45cf0967f29f55f29ca030de93834f
Marcelo De Polli <marcelo.polli@gmail.com>
2013-04-29 11:08:45 -0300

    Initial work on school logo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130409004359) do
+ActiveRecord::Schema.define(:version => 20130426135538) do
 
   create_table "books", :force => true do |t|
     t.datetime "published_at"
@@ -64,9 +64,13 @@ ActiveRecord::Schema.define(:version => 20130409004359) do
     t.integer  "book_id"
     t.date     "release_date"
     t.date     "finish_date"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
     t.integer  "client_id"
+    t.string   "school_logo_file_name"
+    t.string   "school_logo_content_type"
+    t.integer  "school_logo_file_size"
+    t.datetime "school_logo_updated_at"
   end
 
   add_index "projects", ["book_id"], :name => "index_projects_on_book_id"
