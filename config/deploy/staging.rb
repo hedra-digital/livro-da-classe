0c6b8d0afff0ac752deb345c911be16d84f938b6
Marcelo De Polli <marcelo.polli@gmail.com>
2013-04-30 10:18:47 -0300

    Change default staging deploy branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 server '198.211.112.162', :app, :web, :db, :primary => true
 set :deploy_to, "/home/deploy/apps/livrodaclasse_staging"
-set :branch, 'master'
+set :branch, 'staging'
 set(:rails_env) { "#{stage}" }
