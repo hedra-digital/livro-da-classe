726b8cd662361ce7d46399a2d179a7578cbecb43
Marcelo De Polli <marcelo.polli@gmail.com>
2013-04-30 20:26:53 -0300

    Update config.example.yml structure

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -3,5 +3,5 @@ class Admin::ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  http_basic_authenticate_with(:name => CONFIG[:admin_username], :password => CONFIG[:admin_password])
+  http_basic_authenticate_with(:name => CONFIG[:restricted_area][:username], :password => CONFIG[:restricted_area][:password])
 end
