d865de5a47e69898f68a05dee2d5a8e0abd09cdc
Marcelo De Polli <marcelo.polli@gmail.com>
2013-05-07 12:06:32 -0300

    Use with_indifferent_access in initializer

--- config/initializers/01_config.rb
+++ config/initializers/01_config.rb
@@ -1,3 +1,2 @@
-CONFIG = YAML.load_file(File.expand_path('../../config.yml', __FILE__))
+CONFIG = YAML.load_file(File.expand_path('../../config.yml', __FILE__)).with_indifferent_access
 CONFIG.merge! CONFIG.fetch(Rails.env, {})
-CONFIG.symbolize_keys!
