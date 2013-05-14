542da343334f71a5751061c7bead5f85c5f65e26
Marcelo De Polli <marcelo.polli@gmail.com>
2013-05-14 20:15:23 -0300

    Monkey patch convert_img method

--- lib/hedra-latex.rb
+++ lib/hedra-latex.rb
@@ -5,4 +5,16 @@ class HedraLatex < Kramdown::Converter::Latex
     type = @options[:latex_headers][output_header_level(el.options[:level]) - 1]
     return "\\#{type}*{#{inner(el, opts)}}\n\n"
   end
+
+  def convert_img(el, opts)
+    if el.attr['src'] =~ /^(https?|ftps?):\/\//
+      warning("Cannot include non-local image")
+      ''
+    elsif !el.attr['src'].empty?
+      "\\noindent\\nobreak\\dotfill\\par{}Sua imagem estará aqui\\par{}\\noindent\\nobreak\\dotfill"
+    else
+      warning("Cannot include image with empty path")
+      ''
+    end
+  end
 end
