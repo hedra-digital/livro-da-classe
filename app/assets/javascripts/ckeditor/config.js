21002107ce3b74fb1a2b067259fd652d4d34017a
Marcelo De Polli <marcelo.polli@gmail.com>
2013-05-14 22:18:20 -0300

    Revert to full toolbar

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,11 +1,3 @@
 CKEDITOR.editorConfig = function(config) {
-  config.toolbarGroups = [
-    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-    { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-    { name: 'links' },
-    { name: 'insert' },
-    { name: 'document', groups: [ 'mode' ] }
-  ];
 
-  config.language = 'pt-BR';
 };
