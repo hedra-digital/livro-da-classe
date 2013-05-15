e7949d473a2d83c307142f4ee5463751323de15f
Marcelo De Polli <marcelo.polli@gmail.com>
2013-05-14 22:09:12 -0300

    Remove config.js from manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require ckeditor/init
-//= require ckeditor/config.js
 //= require bootstrap-alert
 //= require bootstrap-dropdown
 //= require bootstrap-datepicker/core
