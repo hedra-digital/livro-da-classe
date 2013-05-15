cffc867f7ddf27690121232d860056d0934a17ec
Marcelo De Polli <marcelo.polli@gmail.com>
2013-05-14 21:21:23 -0300

    Minor edit in config.js

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -3,7 +3,8 @@ CKEDITOR.editorConfig = function(config) {
     { name: 'basicstyles', items: [ 'Bold', 'Italic' ] },
     { name: 'insert', items: [ 'Image' ] },
     { name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-    { name: 'code', items: [ 'Source' ] },
+    { name: 'document', items: [ 'Source' ] }
   ];
+
   config.language = 'pt-BR';
-}
+};
