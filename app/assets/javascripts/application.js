7cc285f8b35cf26b656ca01085e924b106bb4d12
Marcelo De Polli <marcelo.polli@gmail.com>
2013-06-18 00:00:38 -0300

    Add link to tutorial on main menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,3 +48,18 @@ $(window).load(function() {
 $(document).ready(function() {
   $('.fileupload').fileupload();
 });
+
+// Tutorial modal
+$(document).ready(function() {
+  $(".tutorial-modal").fancybox({
+    maxWidth    : 800,
+    maxHeight   : 600,
+    fitToView   : false,
+    width       : '70%',
+    height      : '70%',
+    autoSize    : false,
+    closeClick  : false,
+    openEffect  : 'none',
+    closeEffect : 'none'
+  });
+});
