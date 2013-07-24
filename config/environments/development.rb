7107108b89000abc7ccc9c5b26c28c6f5930fa44
Jefferson Fernandes <jeffs.fernandes@gmail.com>
2013-07-24 14:47:47 -0300

    Dev config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,8 @@ Livrodaclasse::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
   # Print deprecation notices to the Rails logger
@@ -35,4 +36,9 @@ Livrodaclasse::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.middleware.use ExceptionNotifier,
+  sender_address: 'jefferson@vizir.com.br',
+  exception_recipients: 'fellipe@vizir.com.br',
+  email_prefix: "LIVRO DA CLASSE" 
 end
