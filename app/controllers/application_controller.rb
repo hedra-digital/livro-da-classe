d3ad72d600eea3d35dde05a9fb3f3412484e7a29
Fellipe <fellipe@vizir.com.br>
2013-08-30 16:16:29 -0300

    Retirando autentificação no staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  http_basic_authenticate_with(:name => CONFIG[:restricted_area][:username], :password => CONFIG[:restricted_area][:password]) if ['staging', 'sales'].include?(Rails.env)
+  http_basic_authenticate_with(:name => CONFIG[:restricted_area][:username], :password => CONFIG[:restricted_area][:password]) if ['sales'].include?(Rails.env)
 
   helper_method :current_user
 
