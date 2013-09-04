aefe0c1bc3d8e07236961c17add3bb47920f9576
Fellipe <fellipe@vizir.com.br>
2013-09-04 11:15:43 -0300

    Atualização para as Specs

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -7,12 +7,12 @@ describe 'unregistered user' do
     it 'visits the home page' do
       visit root_path
       page.should have_selector('h1', :text => 'Livro da Classe')
-      page.should have_link('sign_up', :href => new_user_path)
+      page.should have_link('Cadastrar-se', :href => new_user_path)
     end
 
     it 'clicks the sign up button' do
       visit root_path
-      click_link('sign_up')
+      click_link('Cadastrar-se')
       page.should have_selector('h1', :text => 'Crie sua conta', :visible => true)
       current_path.should eq(new_user_path)
     end
