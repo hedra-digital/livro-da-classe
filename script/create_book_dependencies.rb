6261fdda14403a443d86735c5a8c01d4c336a152
Fellipe <fellipe@vizir.com.br>
2013-10-25 12:31:53 -0200

    Script para atualizar antigas informações do livro

--- script/create_book_dependencies.rb
+++ script/create_book_dependencies.rb
@@ -1,5 +1,9 @@
 Book.all.each do |book|
-  book.build_project quantity: 50 if book.project.nil?
-  book.build_cover_info if book.cover_info.nil?
+  if book.project.nil?
+    book.build_project quantity: 50
+  end
+  if book.cover_info.nil?
+    book.build_cover_info
+  end
   book.save
 end
