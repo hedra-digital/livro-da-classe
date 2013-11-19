8590ac66178d904218bfa24953cea9de45ad6bdf
Fellipe <fellipe@vizir.com.br>
2013-11-19 10:11:36 -0200

    Utilizando o formato novo de criação de pdfs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,18 +40,15 @@ class BooksController < ApplicationController
     @scraps = @book.scraps.order("created_at desc")
     respond_to do |format|
       format.html # show.html.erb
-      format.pdf
-
-      #codigo para novo estilo de geração
-
-      #format.pdf do
-      #  pdf = @book.pdf
-      #  if !pdf.nil?
-      #    send_file(pdf, :filename => "#{@book.uuid}.pdf", :disposition => 'inline', :type => "application/pdf")
-      #  else
-      #    raise "Error in pdf generation"
-      #  end
-      #end
+      #format.pdf
+      format.pdf do
+        pdf = @book.pdf
+        if !pdf.nil?
+          send_file(pdf, :filename => "#{@book.uuid}.pdf", :disposition => 'inline', :type => "application/pdf")
+        else
+          raise "Error in pdf generation"
+        end
+      end
     end
   end
 
