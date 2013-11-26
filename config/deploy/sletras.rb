3fd3d31e82c29686f5f11abfb64556cc3b126f91
Fellipe <fellipe@vizir.com.br>
2013-11-26 16:11:17 -0200

    Configuração inicial 7letras

--- config/deploy/sletras.rb
+++ config/deploy/sletras.rb
@@ -1,4 +1,4 @@
-server '192.241.138.213', :app, :web, :db, :primary => true
+server '198.199.78.184', :app, :web, :db, :primary => true
 set :deploy_to, "/home/deploy/apps/livrodaclasse"
 set :branch, '7letras'
 set(:rails_env) { "#{stage}" }
