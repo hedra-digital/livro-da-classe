4630bdfbee54bfca8ab73391b2bcc408c447f212
Fellipe <fellipe@vizir.com.br>
2013-11-29 10:21:59 -0200

    Trello Mailer com parametro de endereço do site

--- app/mailers/trello_mailer.rb
+++ app/mailers/trello_mailer.rb
@@ -7,7 +7,7 @@ class TrelloMailer < ActionMailer::Base
     @email = user.email
     @telephone = user.telephone
     @abstract = book.abstract
-    @impersonate = "http://#{Livrodaclasse::Application.config.action_mailer.default_url_options[:host]}/admin?impersonate_user_id=#{book.organizer.id}"
+    @impersonate = "http://7letras.livrodaclasse.com.br/admin?impersonate_user_id=#{book.organizer.id}"
     @date = book.created_at.strftime("%d/%m/%Y")
     mail :to => "chagas+eoyipyvrcukn3uslydul@boards.trello.com", :subject => "#{@title} (#{@name})"
   end
