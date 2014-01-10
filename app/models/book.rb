eef14ff8c9939c06763219c1d49cd4bbc09468c5
Fellipe <fellipe@vizir.com.br>
2014-01-10 14:55:26 -0200

    Teste para performance

--- app/models/book.rb
+++ app/models/book.rb
@@ -174,7 +174,7 @@ class Book < ActiveRecord::Base
     else
       pdf_file = nil
       if File.exist?(File.join(directory, 'LIVRO.log'))
-        AdminMailer.delay.pdf_to_latex_error(self, directory, "#{directory}/LIVRO.log")
+        #AdminMailer.delay.pdf_to_latex_error(self, directory, "#{directory}/LIVRO.log")
       end
     end
     pdf_file
