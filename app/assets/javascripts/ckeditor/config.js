0672b20377d3880e962613e7506b3c56fe545777
Fellipe <fellipe@vizir.com.br>
2014-01-29 13:01:27 -0200

    Limpando tags pelo CKEditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -80,16 +80,14 @@ CKEDITOR.editorConfig = function(config) {
   // Because of 
   config.hideDialogFields = "image:info:htmlPreview";
 
-  config.allowedContent = true;
-  /*    'h1 h2 h3 h4 h5 h6 p blockquote strong em;' +
-      'a[!href];' +
+  config.allowedContent = 'h1 h2 h3 h4 h5 h6 p blockquote strong em;' +
+      'a[!href,name];' +
       'img(left,right)[!src,alt,width,height];' +
       'table tr th td caption;' +
       'span{!font-family};' +
       'span{!color};' +
       'span(!marker);' +
-      'del ins'
-    */
+      'div[id]';
 
   // Rails CSRF token
   config.filebrowserParams = function(){
