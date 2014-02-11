670f4b7b51911de2a950f8e03d0a65e57ea060e8
Fellipe <fellipe@vizir.com.br>
2014-02-11 18:22:29 -0200

    Teste

--- app/models/book.rb
+++ app/models/book.rb
@@ -143,11 +143,11 @@ class Book < ActiveRecord::Base
   end
 
   def autor
-    self.book_data.nil? or self.book_data.autor.nil? ? self.organizer.name : self.book_data.autor
+    self.book_data.nil? or self.book_data.autor.nil? ? "" : "#{self.book_data.autor}-"
   end
 
   def directory_name
-    "#{String.remover_acentos(self.autor).gsub(/[^0-9A-Za-z]/, '')}-#{String.remover_acentos(self.title).gsub(/[^0-9A-Za-z]/, '')}-#{self.template}-#{self.id}"
+    "#{String.remover_acentos(self.autor).gsub(/[^0-9A-Za-z]/, '')}#{String.remover_acentos(self.title).gsub(/[^0-9A-Za-z]/, '')}-#{self.template}-#{self.id}"
   end
 
   def check_repository
