36b593b35c22da5876abb872053cbd482d10ae08
Fellipe <fellipe@vizir.com.br>
2014-02-11 14:49:02 -0200

    Ajuste em configuração

--- app/models/version.rb
+++ app/models/version.rb
@@ -2,7 +2,7 @@ class Version
 
   def self.commit_directory directory, user_profile, directory_name
     system "curl --user tipografiadigital:hedra21 https://api.bitbucket.org/1.0/repositories/ --data name=#{directory_name}"
-    system "cd #{directory}/ && git init && git remote add origin #{CONFIG[Rails.env.to_sym]["git_url"]}/#{directory_name}.git && git add . && git commit -a -m \"#{user_profile}\" && git push origin master"
+    system "cd #{directory}/ && git init && git remote add origin #{CONFIG[Rails.env.to_sym]["git"]}/#{directory_name}.git && git add . && git commit -a -m \"#{user_profile}\" && git push origin master"
   end
 
   def self.commit_file directory, text, user_profile, message
