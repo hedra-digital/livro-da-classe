a922614a3ff8ca326d959f60922aa0af1e8fe671
Fellipe <fellipe@vizir.com.br>
2014-02-12 10:38:54 -0200

    Correção em visualização da capa

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -41,6 +41,8 @@ module BooksHelper
   def book_cover
     if @book.book_data.capainteira.exists?
       link_to(image_tag(@book.book_data.capainteira.url(:thumb)), @book.book_data.capainteira.url(:content), :class => 'cover-modal')
+    elsif @book.cover.exists?
+      link_to(image_tag(@book.cover.url(:thumb)), @book.cover.url(:content), :class => 'cover-modal')
     elsif !DefaultCover.first.nil?
       link_to(image_tag(DefaultCover.first.default_cover.url(:thumb)), DefaultCover.first.default_cover.url, :class => 'cover-modal')
     else
