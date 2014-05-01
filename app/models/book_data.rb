12e5c77a4cdaa3128f02a10b2bf1ad059f676a2a
Andy <Andy>
2014-05-02 00:15:19 +0800

    fixed bug

--- app/models/book_data.rb
+++ app/models/book_data.rb
@@ -87,7 +87,7 @@ class BookData < ActiveRecord::Base
                           :small => ["300x300#", :png]
                     }
 
-  validates_attachment_content_type :logo, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif|svg|tif)$/, :message => 'O arquivo anexado não é compatível. Tente novamente ou entre em contato com nossos editores.'
+  validates_attachment_content_type :logo, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif|svg+xml|tiff)$/, :message => 'O arquivo anexado não é compatível. Tente novamente ou entre em contato com nossos editores.'
 
   has_attached_file :imagemficha,
                     :styles => {
