e5e4f93296fd4ffd3b05d327e6df3c82fad2cca2
Andy <Andy>
2014-05-17 22:59:23 +0800

    2.0.3 #116 https://trello.com/c/YpQu9YIW New features for split chapter

--- app/assets/javascripts/ckeditor/plugins/latexformater/plugin.js
+++ app/assets/javascripts/ckeditor/plugins/latexformater/plugin.js
@@ -8,7 +8,7 @@ CKEDITOR.plugins.add('latexformater',
     strings.push(['versos', 'versos', 'formata versos']);
     strings.push(['epígrafe', 'epígrafe', 'insere epígrafe']);
     strings.push(['nota de rodapé', 'nota de rodapé', 'insere uma nota de rodapé']);
-    strings.push(['novo capítulo', 'novo capítulo', 'insere novo capítulo']);
+    //strings.push(['novo capítulo', 'novo capítulo', 'insere novo capítulo']);
     strings.push(['espaço pequeno', 'espaço pequeno', 'espaço pequeno entrelinhas']);
     strings.push(['espaço médio', 'espaço médio', 'espaço médio entrelinhas']);
     strings.push(['espaço grande', 'espaço grande', 'espaço grande entrelinhas']);
@@ -45,7 +45,7 @@ CKEDITOR.plugins.add('latexformater',
         }else if(value == 'nota de rodapé'){
           CKEDITOR.currentInstance.commands.footnoteDialog.exec()
         }else if(value == 'novo capítulo'){
-          CKEDITOR.currentInstance.commands.chapterDialog.exec()
+          // CKEDITOR.currentInstance.commands.chapterDialog.exec()
         }else if(value == 'espaço pequeno'){
           CKEDITOR.currentInstance.commands.insertSmallskip.exec()
         }else if(value == 'espaço médio'){
--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -9,7 +9,6 @@ class TextsController < ApplicationController
 
   def show
     @text = Text.find_by_uuid_or_id(params[:id])
-    @book = @text.book
     @comment = Comment.new
   end
 
