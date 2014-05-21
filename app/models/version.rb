d6403c3d2ec6dc1e40af5e078684769075f464fa
Andy <Andy>
2014-05-22 01:25:23 +0800

    2.0.3 #120 https://trello.com/c/f9dvVxSy changes in git feature

--- app/models/version.rb
+++ app/models/version.rb
@@ -3,7 +3,6 @@ class Version
   def self.commit_directory directory, user_profile, directory_name
     system "curl --user #{CONFIG[Rails.env.to_sym]["git_user_pass"]} https://api.bitbucket.org/1.0/repositories/ --data name=#{directory_name} --data owner=#{CONFIG[Rails.env.to_sym]["git_team"]} --data is_private=true"
     system "cd #{directory}/ && git init && git remote add origin #{CONFIG[Rails.env.to_sym]["git"]}/#{directory_name}.git && git add . && git commit -a -m \"#{user_profile}\" && git push origin master"
-    Version.add_to_submodule directory_name
   end
 
   def self.commit_file directory, text, user_profile, user_name, message
@@ -11,12 +10,6 @@ class Version
     message = ":: #{message}" unless message.blank?
     filename = File.join(Rails.root, text.filename)
     system "cd #{directory}/ && git pull origin master && git add . && git commit -a -m \"#{user_profile} (#{user_name}) #{message}\""
-  end
-
-  def self.add_to_submodule directory_name
-    submodule_directory = CONFIG[Rails.env.to_sym]["books_submodule_path"]
-    git_repo = "#{CONFIG[Rails.env.to_sym]["git"]}/#{directory_name}.git"
-    system "cd #{submodule_directory}/ && git submodule add #{git_repo} && git add . && git commit -a -m \"New Book -> #{directory_name}\" && git push origin master"
   end
 
   def self.push_to_repository directory
