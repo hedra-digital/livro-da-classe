165f9357c3f5f9a57195b81ea638fa255aa8c2b3
Andy <Andy>
2014-05-24 01:09:37 +0800

    2.0.3 #137 https://trello.com/c/sX6lBw7s Rebuild footnote

--- app/assets/javascripts/ckeditor/plugins/footnote/plugin.js
+++ app/assets/javascripts/ckeditor/plugins/footnote/plugin.js
@@ -1,13 +1,24 @@
 CKEDITOR.plugins.add( 'footnote', {
-    icons: 'footnote',
-    init: function( editor ) {
-        editor.addCommand( 'footnoteDialog', new CKEDITOR.dialogCommand( 'footnoteDialog' ) );
-        editor.ui.addButton( 'FootNote', {
-            label: 'Insere uma nota de rodapé',
-            command: 'footnoteDialog',
-            toolbar: 'insert'
-        });
+  icons: 'footnote',
+  init: function( editor ) {
+    editor.addCommand( 'footnoteDialog', new CKEDITOR.dialogCommand( 'footnoteDialog' ) );
+    editor.ui.addButton( 'Footnote', {
+        label: 'Insere uma nota de rodapé',
+        command: 'footnoteDialog',
+        toolbar: 'insert'
+    });
 
-        CKEDITOR.dialog.add( 'footnoteDialog', this.path + 'dialogs/footnote.js' );
-    }
+    CKEDITOR.dialog.add( 'footnoteDialog', this.path + 'dialogs/footnote.js' );
+
+    // add the plugin css
+    if (typeof CKEDITOR.config.contentsCss === 'string') {
+      CKEDITOR.config.contentsCss = [CKEDITOR.config.contentsCss];
+    } 
+
+    CKEDITOR.config.contentsCss.push(CKEDITOR.basePath + CKEDITOR.plugins.basePath + 'footnote/plugin.css');
+
+    // walk around for the image path
+    CKEDITOR.addCss('a.footnote {background-image: url(' + CKEDITOR.getUrl(this.path + 'icons/footnote_red.png') + ');}')
+
+  }
 });
