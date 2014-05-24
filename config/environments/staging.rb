b889cf81201e2d0e30404936a6ffdf4e656ee091
Andy <Andy>
2014-05-25 01:36:23 +0800

    2.0.3 #137 https://trello.com/c/sX6lBw7s Rebuild footnote

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,6 +47,7 @@ Livrodaclasse::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( public.css )
+  config.assets.precompile += ['ckeditor/*']
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
