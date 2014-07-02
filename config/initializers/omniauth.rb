b2919f5e50f4f753ec2a6a18e6d4cd634412f274
Andy <andylinid45@users.noreply.github.com>
2014-07-02 17:47:55 +0800

    2.0.4 https://trello.com/c/GD8AchTJ  Create a new environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,33 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
-if Rails.env.production?
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, "2byTz4MOQIyv3YoMHzFVEQ", "cRU6vQSh7pg5yZxRRPSu1HnSFNDSZI4SDmeo0ZcVV5I"
-    provider :facebook, "223113251187371", "00774062f4529edff4e0f93f257a90de"
-    provider :google_oauth2, "41386639646.apps.googleusercontent.com", "pOgZZAOFIpJEA6CqrknsyQJv"
-  end
-elsif Rails.env.sales?
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, "g2pgUJetYRoDTePZ4TcyQ", "LkRyPuYWRT2g0bq543E3uKbjoDrcUkQCZImZfLHqJg"
-    provider :facebook, "462817203782895", "6ad0fab174bcd0d298195957fe68a48c"
-    provider :google_oauth2, "1087006017149-t8ad3ej20ib46kqs33638avmsu7kr3fo.apps.googleusercontent.com", "Z1C7hirHxUe25zgLq5oGshsC"
-  end
-elsif Rails.env.staging? || Rails.env.test? || Rails.env.development?
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, "HFwmE8CdAqLrgMjQROw8g", "FjHG7WEOEGF9zS15UrK1DS2bKFuVVcLsPzVJasqpU"
-    provider :facebook, "223113251187371", "00774062f4529edff4e0f93f257a90de"
-    provider :google_oauth2, "1087006017149-trcfrjjs4fg5ftjsmp8dpd1oai84vifu.apps.googleusercontent.com", "1maEZuCXHaFwYiQF_cXIjBY2"
-  end
-elsif Rails.env.sletras?
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, "NnjbyC3WP9r8sJKnnRJDw", "Nha7HvAy95QdY8SsT45HHYhDTbmK53XI5wMkChLMyzI"
-    provider :facebook, "1450227325205442", "6a5fe19a02b4cc55538dd27296b6911f"
-    provider :google_oauth2, "74091404785.apps.googleusercontent.com", "jG8HtUkT5RuL4z5AdeJMi1kI"
-  end
-elsif Rails.env.tipostaging?
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, "NnjbyC3WP9r8sJKnnRJDw", "Nha7HvAy95QdY8SsT45HHYhDTbmK53XI5wMkChLMyzI"
-    provider :facebook, "1450227325205442", "6a5fe19a02b4cc55538dd27296b6911f"
-    provider :google_oauth2, "74091404785.apps.googleusercontent.com", "jG8HtUkT5RuL4z5AdeJMi1kI"
-  end
-end
+Rails.application.config.middleware.use OmniAuth::Builder do 
+  omni_auth_config = YAML.load_file(File.join(Rails.root, "config", "config.yml"))[Rails.env]["providers"]        
+  omni_auth_config.each do |name, credentials|  
+    provider name.to_sym, credentials["key"], credentials["secret"], {:client_options => omni_auth_config[:client_options]}         
+  end  
+end  
