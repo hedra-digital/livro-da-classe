a719a683f61285ff39460d80ea096233c22dc5db
Andy <andylinid45@users.noreply.github.com>
2014-07-02 18:34:20 +0800

    2.0.4 https://trello.com/c/GD8AchTJ  Create a new environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,6 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do 
   omni_auth_config = YAML.load_file(File.join(Rails.root, "config", "config.yml"))[Rails.env]["providers"]        
   omni_auth_config.each do |name, credentials|  
-    provider name.to_sym, credentials["key"], credentials["secret"], {:client_options => omni_auth_config[:client_options]}         
+    provider name.to_sym, credentials["key"], credentials["secret"]         
   end  
 end  
