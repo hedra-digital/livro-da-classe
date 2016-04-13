222ca31b09ca48654fe550847603a5f55764892b
Abner <pauloabner@gmail.com>
2016-04-12 21:59:00 -0300

    Convert hyperlink to footnotes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -217,6 +217,7 @@ class BooksController < ApplicationController
     doc = Nokogiri::HTML(content)
     doc = remove_head(doc)
     doc = replace_ol_h1(doc)
+    doc = convert_footnotes(doc)
     doc.to_html
   end
 
@@ -237,5 +238,64 @@ class BooksController < ApplicationController
   def remove_head(content)
     content.css('head').remove
     content
+  end
+
+  def convert_footnotes(doc)
+    contents = doc.css('a').select {
+      |a| footnote_content?(a)
+    }
+
+    doc.css('a').each do |a|
+      if footnote?(a)
+        ftntc = contents.select { |ftntc| footnote_ref(a) == footnote_content_ref(ftntc) }
+        new_a = create_footnote(a)
+
+        sup = Nokogiri::XML::Node.new 'sup', new_a
+        sup.content = '*'
+        sup.parent = new_a
+
+        div = Nokogiri::XML::Node.new 'div', new_a
+        div['class'] = 'sdfootnotesym'
+        div['data-id'] = footnote_content_ref(ftntc.first)
+        new_a.add_next_sibling(div)
+
+        p_text = Nokogiri::XML::Node.new 'p', div
+        p_text.content = footnote_content(ftntc)
+        p_text.parent = div
+
+        a = new_a
+      end
+    end
+    contents.each { |c| c.parent.remove }
+    doc
+  end
+
+  def footnote_content?(a)
+    a.attributes['href'].value.include?('ftnt_ref') && a.parent.name == 'p'
+  end
+
+  def footnote?(a)
+    a.attributes['id'].value.include?('ftnt_ref') && a.parent.name == 'sup'
+  end
+
+  def footnote_ref(a)
+    a.attributes['href'].value.delete('#')
+  end
+
+  def footnote_content_ref(ftnt_content)
+    ftnt_content.attributes['id'].value
+  end
+
+  def footnote_content(ftnt_content)
+    ftnt_content.first.parent.children.last.text.gsub(/\u00a0/, '').strip
+  end
+
+  def create_footnote(a)
+    a['class'] = 'sdfootnoteanc'
+    a['data-id'] = footnote_ref(a)
+    a.attributes['href'].remove
+    a.children.remove
+    a.attributes['id'].remove
+    a
   end
 end
