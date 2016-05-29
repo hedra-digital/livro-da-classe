f85f030220781a8ad8c6c2857c77627c6b50bee9
Paulo Abner <pauloabner@gmail.com>
2016-05-29 15:42:25 -0300

    Save commands.sty

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -1,7 +1,24 @@
 # Rule Model
 class Rule < ActiveRecord::Base
+  after_save :generate_commands
   attr_accessible :label, :command
 
   validates                 :label,     presence: true, length: { maximum: 30 }
   validates                 :command,   presence: true
+
+  private
+
+  def generate_commands
+    Thread.new do
+      logger.info 'Update commands.sty'
+      input_commands = ''
+      Rule.all.each do |rule|
+        if rule.active
+          input_commands << "% #{rule.label}\n"
+          input_commands << rule.command + "\n\n"
+        end
+      end
+      File.open('public/commands.sty','w') {|io| io.write(input_commands) }
+    end
+  end
 end
