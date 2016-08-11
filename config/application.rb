1b90637e4d29875acddab632b96cf82b3809ab13
Paulo Abner <pauloabner@gmail.com>
2016-08-10 23:54:52 -0300

    Added internationalization and es language

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Livrodaclasse
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.available_locales = [:en, :"pt-BR"]
+    config.i18n.available_locales = [:es, :en, :"pt-BR"]
     config.i18n.default_locale = :"pt-BR"
     config.i18n.locale = :"pt-BR"
 
