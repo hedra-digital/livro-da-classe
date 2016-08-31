c6be5d99f32fd3927b67cb265b25de9988e53476
Rodrigo Galba <rodrigogalba@gmail.com>
2016-08-31 18:26:02 -0300

    fixing README. Adding default user creation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@ Profile.create(:desc => "Publisher")
 BookStatus.create(:desc => "Em fila")
 BookStatus.create(:desc => "Aprovado")
 BookStatus.create(:desc => "Em impressão")
+
+User.create(name: "Admin", email: "admin@example.com", password: "password", profile: Profile.find(2))
